1884ac16a0135acfa87b73962061a9704c170938
jmvazquez <juan@vazquez.bio>
2017-01-12 16:33:54 -0600

    v0.2 - Began work on RecBlast2, based on the SearchIO package (looks cleaner) - Fixed the forward BLAST section of RecBlast to conform to PEP8 - Fixed file nomenclature to make I/O more legible
    Signed-off-by: docmanny <juan@vazquez.bio>

--- modules.py
+++ modules.py
@@ -19,64 +19,77 @@ the sequence of choice from Species 1 to Species 2; it then takes all hits above
 and stores them in Fasta format. Then, for each hit, it will blast the FASTA sequence of Species 2 against Species 1;
 it will then obtain the names of the top hits and append them in order to the header of the Species 2 FASTA sequence.
 """
-def RecBlast(seqfile, species2, database1, filetype="fasta",species1="Homo Sapiens",blasttype='blastp',localblast1=True,
-             localblast2=False,database2="refseq_proteins",evalue=0.001,identitiesperc=0.75,scoreperc=0.75,
-             lengthperc=0.75,idthres='Score'):
+
+
+def RecBlast(seqfile, species2, database1, filetype="fasta", species1="Homo Sapiens", blasttype='blastp',
+             localblast1=True, localblast2=False, database2="refseq_proteins", evalue=0.001, identitiesperc=0.75,
+             scoreperc=0.75, lengthperc=0.75, idthres='Score'):
     import os
     from Bio import SeqIO
     from Bio.Blast import NCBIWWW, NCBIStandalone
-    from Bio.Blast.Applications import NcbiblastxCommandline,NcbiblastnCommandline,NcbiblastpCommandline,NcbitblastxCommandline,NcbitblastnCommandline
+    from Bio.Blast.Applications import NcbiblastxCommandline,NcbiblastnCommandline,NcbiblastpCommandline, \
+        NcbitblastxCommandline,NcbitblastnCommandline
     print("Now starting...")
     # First loop will iterate over each sequence in a file, preferably FASTA but also allows for GenBank
     for index, seq_record in enumerate(SeqIO.parse(seqfile, filetype)):
         print("{}: {}".format(index,seq_record.name))
 
-        # outfile is the file where the final Blast output will go to for this particular sequence. This is the annotated
-            # FASTA, which will contain the top hits of the first BLAST, annotated with the name of the second BLAST
-        # errorfile is the error log for the first BLAST parser, which will be used to get top hits for the reciprocal
-            # BLAST
-        # errorfile2 is the error log for the second BLAST parser, which will be used to get the top hits of the
-            # reciprocal BLAST, which then will be used to annotate the hits from the primary BLAST
-        outfile = os.path.join(os.getcwd(), "{}_recblast_out".format(species2), "{0}_{1}.fasta".format(blasttype,seq_record.name))
-        errorfile = open(os.path.join(os.getcwd(), "recblast_out", "{}_recblasterror1.log".format(seqfile)),"w+")
-        errorfile2 = open(os.path.join(os.getcwd(), "recblast_out", "{}_recblasterror2.log".format(seqfile)), "w+")
+        """
+        recblast_out is the file where the final Blast output will go to for this particular sequence. This is the
+        annotated FASTA, which will contain the top hits of the first BLAST, annotated with the name of the
+        second BLAST
+        """
+        recblast_out = os.path.join(os.getcwd(),
+                                    "{0}_recblast_out".format(species2),
+                                    "{0}_{1}.fasta".format(blasttype, seq_record.name))
 
-        # Begin by opening outfile, and then start with the primary BLAST
-        with open(outfile, "w+") as blastout:
+        # Begin by opening recblast_out, and then start with the primary BLAST
+        with open(recblast_out, "w+") as blastout:
             length = len(seq_record)
             if localblast1:
                 if blasttype == "blastn":
-                    NcbiblastnCommandline(query=seq_record.seq,db=database1,evalue=evalue,outfmt=5,
-                                          out="{1}to{0}blast_{2}.xml".format(species2, species1,
+                    NcbiblastnCommandline(query=seq_record.seq, db=database1, evalue=evalue, outfmt=5,
+                                          out="{0}to{1}blast_{2}.xml".format(species1, species2,
                                                                              seq_record.name))
                 elif blasttype == "blastp":
-                    NcbiblastpCommandline(query=seq_record.seq,db=database1,evalue=evalue,outfmt=5,
-                                          out="{1}to{0}blast_{2}.xml".format(species2, species1,
+                    NcbiblastpCommandline(query=seq_record.seq, db=database1, evalue=evalue, outfmt=5,
+                                          out="{0}to{1}blast_{2}.xml".format(species1, species2,
                                                                              seq_record.name))
                 elif blasttype == "blastx":
-                    NcbiblastxCommandline(query=seq_record.seq,db=database1,evalue=evalue,outfmt=5,
-                                          out="{1}to{0}blast_{2}.xml".format(species2, species1,
+                    NcbiblastxCommandline(query=seq_record.seq, db=database1, evalue=evalue, outfmt=5,
+                                          out="{0}to{1}blast_{2}.xml".format(species1, species2,
                                                                              seq_record.name))
                 elif blasttype == "tblastx":
-                    NcbitblastxCommandline(query=seq_record.seq,db=database1,evalue=evalue,outfmt=5,
-                                           out="{1}to{0}blast_{2}.xml".format(species2, species1,
+                    NcbitblastxCommandline(query=seq_record.seq, db=database1, evalue=evalue, outfmt=5,
+                                           out="{0}to{1}blast_{2}.xml".format(species1, species2,
                                                                               seq_record.name))
                 elif blasttype == "tblastn":
-                    NcbitblastnCommandline(query=seq_record.seq,db=database1,evalue=evalue,outfmt=5,
-                                           out="{1}to{0}blast_{2}.xml".format(species2, species1,
+                    NcbitblastnCommandline(query=seq_record.seq, db=database1, evalue=evalue, outfmt=5,
+                                           out="{0}to{1}blast_{2}.xml".format(species1, species2,
                                                                               seq_record.name))
                 else:
                     raise Exception("Invalid blast choice!")
             else:
-                with open(os.path.join(os.getcwd(), "recblast_out", "{1}to{0}blast_{2}.xml".format(species2, species1,
-                                        seq_record.name)), "w+") as fxml:
-                    fxml.write(NCBIWWW.qblast(program=blasttype,database=database1,sequence=seq_record.seq,
-                                              entrez_query=(species2 + "[ORGN]"),expect=evalue))
+                with open(os.path.join(os.getcwd(),
+                                       "recblast_out",
+                                       "{0}to{1}blast_{2}.xml".format(species1, species2, seq_record.name)),
+                          "w+") as fxml:
+                    fxml.write(NCBIWWW.qblast(program=blasttype, database=database1, sequence=seq_record.seq,
+                                              entrez_query=(species2 + "[ORGN]"), expect=evalue))
 
             # Start Reciprocal Blast
             # First load the primary BLAST hits to a handle.
-            blasthits = open("{1}to{0}blast_{2}.xml".format(species2, species1, seq_record.name),"r")
-            # Next, load up the error parsers
+            blasthits = open("{0}to{1}blast_{2}.xml".format(species1, species2, seq_record.name),"r")
+
+            """
+            Next, load up the error parsers:
+                errorfile is the error log for the first BLAST parser, which will be used to get top hits of the
+                        forward direction BLAST, for use in the reverse BLAST
+                errorfile2 is the error log for the second BLAST parser, which will be used to get the top hits of the
+                        reverse BLAST, which then will be used to annotate the hits from the forward BLAST
+            """
+            errorfile = open(os.path.join(os.getcwd(), "recblast_out", "{}_recblasterror1.log".format(seqfile)), "w+")
+            errorfile2 = open(os.path.join(os.getcwd(), "recblast_out", "{}_recblasterror2.log".format(seqfile)), "w+")
             blast_error_parser = NCBIStandalone.BlastErrorParser(errorfile)
             blast_error_parser2 = NCBIStandalone.BlastErrorParser(errorfile2)
 
@@ -93,7 +106,7 @@ def RecBlast(seqfile, species2, database1, filetype="fasta",species1="Homo Sapie
                     for hsp in alignment.hsps:
                         if (hsp.score >= (scoreperc * topscore) & hsp.expect <= evalue &
                             hsp.align_length >= (length * lengthperc) & hsp.identities >=identitiesperc):
-                            
+
                             if localblast2:
                                 if blasttype == "blastn":
                                     NcbiblastnCommandline(query=hsp.query,db=database2,evalue=evalue,outfmt=5,
@@ -122,7 +135,7 @@ def RecBlast(seqfile, species2, database1, filetype="fasta",species1="Homo Sapie
                                         species2,species1,seq_record.name), "w+")) as fxml:
                                     fxml.write(NCBIWWW.qblast(program=blasttype,database=database2,sequence= hsp.query,
                                                entrez_query=(species1 + "[ORGN]"),expect=evalue))
-                            recblasthits = open("{0}to{1}blast_{2}.xml".format(species2, species1, seq_record.name))
+                            recblasthits = open("{1}to{0}blast_{2}.xml".format(species1, species2, seq_record.name))
                             for recblastrecord in NCBIStandalone.Iterator(recblasthits, blast_error_parser2):
                                 hit_names = []
                                 if str(idthres).isnumeric():
@@ -157,3 +170,79 @@ def RecBlast(seqfile, species2, database1, filetype="fasta",species1="Homo Sapie
             errorfile.close()
             errorfile2.close()
 
+
+"""
+RecBlast2 uses the experimental Biopython SearchIO module. Honestly it looks so much cleaner from the docs so I kinda
+jumped ship to fix RecBlast without the whole "for x in y / for y in z / for z in a / ..." hell.
+"""
+
+
+def RecBlast2(seqfile, species2, database1, filetype="fasta", species1="Homo Sapiens", blasttype='blastp',
+              localblast1=True, localblast2=False, database2="refseq_proteins", evalue=0.001, identitiesperc=0.75,
+              scoreperc=0.75, lengthperc=0.75, idthres='Score'):
+    import os
+    from Bio import SeqIO
+    from Bio.Blast import NCBIWWW, NCBIStandalone
+    from Bio.Blast.Applications import NcbiblastxCommandline,NcbiblastnCommandline,NcbiblastpCommandline,NcbitblastxCommandline,NcbitblastnCommandline
+    print("Now starting...")
+    # First loop will iterate over each sequence in a file, preferably FASTA but also allows for GenBank
+    for index, seq_record in enumerate(SeqIO.parse(seqfile, filetype)):
+        print("{}: {}".format(index,seq_record.name))
+
+        # recblast_out is the file where the final Blast output will go to for this particular sequence. This is the annotated
+        #  FASTA, which will contain the top hits of the first BLAST, annotated with the name of the second BLAST
+        # errorfile is the error log for the first BLAST parser, which will be used to get top hits for the reciprocal
+        #  BLAST
+        # errorfile2 is the error log for the second BLAST parser, which will be used to get the top hits of the
+        #  reciprocal BLAST, which then will be used to annotate the hits from the primary BLAST
+        recblast_out = os.path.join(os.getcwd(), "{}_recblast_out".format(species2), "{0}_{1}.fasta".format(blasttype,seq_record.name))
+        errorfile = open(os.path.join(os.getcwd(), "recblast_out", "{}_recblasterror1.log".format(seqfile)),"w+")
+        errorfile2 = open(os.path.join(os.getcwd(), "recblast_out", "{}_recblasterror2.log".format(seqfile)), "w+")
+
+        # Begin by opening recblast_out, and then start with the primary BLAST
+        with open(recblast_out, "w+") as blastout:
+            length = len(seq_record)
+            if localblast1:
+                if blasttype == "blastn":
+                    NcbiblastnCommandline(query=seq_record.seq,db=database1,evalue=evalue,outfmt=5,
+                                          out="{0}to{1}blast_{2}.xml".format(species1, species2,
+                                                                             seq_record.name))
+                elif blasttype == "blastp":
+                    NcbiblastpCommandline(query=seq_record.seq,db=database1,evalue=evalue,outfmt=5,
+                                          out="{0}to{1}blast_{2}.xml".format(species1, species2,
+                                                                             seq_record.name))
+                elif blasttype == "blastx":
+                    NcbiblastxCommandline(query=seq_record.seq,db=database1,evalue=evalue,outfmt=5,
+                                          out="{0}to{1}blast_{2}.xml".format(species1, species2,
+                                                                             seq_record.name))
+                elif blasttype == "tblastx":
+                    NcbitblastxCommandline(query=seq_record.seq,db=database1,evalue=evalue,outfmt=5,
+                                           out="{0}to{1}blast_{2}.xml".format(species1, species2,
+                                                                              seq_record.name))
+                elif blasttype == "tblastn":
+                    NcbitblastnCommandline(query=seq_record.seq,db=database1,evalue=evalue,outfmt=5,
+                                           out="{0}to{1}blast_{2}.xml".format(species1, species2,
+                                                                              seq_record.name))
+                else:
+                    raise Exception("Invalid blast choice!")
+            else:
+                with open(os.path.join(os.getcwd(), "recblast_out", "{0}to{1}blast_{2}.xml".format(species1, species2,
+                                        seq_record.name)), "w+") as fxml:
+                    fxml.write(NCBIWWW.qblast(program=blasttype,database=database1,sequence=seq_record.seq,
+                                              entrez_query=(species2 + "[ORGN]"),expect=evalue))
+
+            # Start Reciprocal Blast
+            # First load the primary BLAST hits to a handle.
+            blasthits = open("{0}to{1}blast_{2}.xml".format(species1, species2, seq_record.name),"r")
+            # Next, load up the error parsers
+            blast_error_parser = NCBIStandalone.BlastErrorParser(errorfile)
+            blast_error_parser2 = NCBIStandalone.BlastErrorParser(errorfile2)
+
+            # Iterate through all the blast hits
+
+
+""""
+SirBlastALot() is a function that, for a given sequence, BLASTS it against a list of organisms of one's choice; the
+default list has an extensive vertebrate coverage. It also has an option to use BLAT instead. By default it just does
+does a unidirectional blast of your sequence to each organism; you can set it to do a Reciprocal Blast as well.
+"""
