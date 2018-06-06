0518aeb02a9e85debf4c3848c4a71cf6a554b4ec
docmanny <juan@vazquez.bio>
2018-06-06 14:12:07 -0500

    setup.py fixed
    Signed-off-by: docmanny <juan@vazquez.bio>

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 from distutils.core import setup
+from RecBlast import __version__
 setup(name='RecBlast',
-      version='1.4dev0',
+      version=__version__,
       description='A flexible module to perform Reciprocal Best-Hit BLAST searches',
       author='Juan Manuel Vazquez',
       author_email='juan@vazquez.bio',
-      url='https://www.vazquez.bio/RecBlastRun',
+      url='https://www.vazquez.bio/RecBlast',
       install_requires=['biopython>=1.69', 'dill>=0.2.6', 'multiprocess', 'mygene'],
-      py_modules=['RecBlast','Auxilliary', 'FetchSeq', "RBC", "Search", "test", "WarningsExceptions"])
+      packages=['RecBlast'])
